0e6219a04b4ff1b7cbacc1472033a0627da1bc8e
Wander van der Wal <wvdwal@hotmail.nl>
2016-04-25 12:08:38 +0200

    Removed the error creating startup message

--- Code/Main.py
+++ Code/Main.py
@@ -1,7 +1,6 @@
 import telegram
 from telegram.ext import Updater
-import Commands
-import sys
+import Commands as Command
 
 
 bot = telegram.Bot(token='210767489:AAG1Hfr1e3gdI7Ib6XiCB8Ff5pbFEhvgrrU')
@@ -10,16 +9,7 @@ print(bot.getMe())
 updates = bot.getUpdates()
 print([u.message.text for u in updates])
 
-try:
-    chat_id = bot.getUpdates()[-1].message.chat_id
-except:
-    sys.exit("Error: make sure there are new messages, before starting the bot")
-
-# except:
-# print("Game breaking error")
-bot.sendMessage(chat_id=chat_id, text="Hallo, ik ben een NHL bot die jou probeert te helpen om te starten met jou studie.")
-
-questions = Commands.Questions
+questions = Command.Questions
 
 def echo(bot, update):
     questions.money(bot, update)
