e3b2b3a64cf54d3bfa11cef748795a397aa07076
Wander van der Wal <wvdwal@hotmail.nl>
2016-05-17 13:11:34 +0200

    Added the Add Command

--- Code/Admin.py
+++ Code/Admin.py
@@ -8,12 +8,20 @@ updater.start_polling()
 Database_Primary = Database("Answers.txt")
 Database_Secondary = Database("Basic.txt")
 
-def result(update):
-    msg = update.message.text.lower()
-    if msg.startswith("/add "):
-        command_add(msg[4:])
+class Controller:
+    def admin(bot,update):
+        bot.chat_id = update.message.chat_id
+        msg = update.message.text.lower()
+        print("Admin Command: " + msg)
+        if msg.startswith("#add "):
+            bot.sendMessage(chat_id=bot.chat_id, text="Thanks for making me smarter")
+            command_add(msg[5:])
+
+    dispatcher.addTelegramMessageHandler(admin)
 
 def command_add(msg):
     arr = msg.split("||")
     key = arr[0]
     ans = arr[1]
+    Database_Primary.add(key,ans)
+    print("Added Response: "+msg)
--- Code/Main.py
+++ Code/Main.py
@@ -1,7 +1,7 @@
 import telegram
 from telegram.ext import Updater
 import Commands
-import Admin as admin
+import Admin
 
 user_bot = telegram.Bot(token='210767489:AAG1Hfr1e3gdI7Ib6XiCB8Ff5pbFEhvgrrU')
 admin_bot = telegram.Bot(token='224044671:AAG8-QYX2obXKvUpHcXVwJxM2Yk4WwsouxE')
@@ -11,17 +11,10 @@ print(admin_bot.getMe())
 bot_updates = user_bot.getUpdates()
 admin_updates = admin_bot.getUpdates();
 print([u.message.text for u in bot_updates])
+print([u.message.text for u in admin_updates])
 
 questions = Commands.Questions
-
-
-def echo(bot, update):
-    #User
-    if bot.id == 210767489:
-        questions.result(bot, update)
-    #Admin
-    if bot.id == 224044671:
-        admin.result(bot, update)
+admin = Admin.Controller
 
 
 # from Commands import Basic
