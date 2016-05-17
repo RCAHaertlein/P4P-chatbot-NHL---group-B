90b58f4b004c94534e9bda3ca5d28751970dce34
Ruud Haertlein <Ruud Haertlein>
2016-05-17 12:55:38 +0200

    Merge

--- Code/Main.py
+++ Code/Main.py
@@ -1,20 +1,28 @@
 import telegram
 from telegram.ext import Updater
 import Commands
+import Admin as admin
 
+user_bot = telegram.Bot(token='210767489:AAG1Hfr1e3gdI7Ib6XiCB8Ff5pbFEhvgrrU')
+admin_bot = telegram.Bot(token='224044671:AAG8-QYX2obXKvUpHcXVwJxM2Yk4WwsouxE')
 
-bot = telegram.Bot(token='210767489:AAG1Hfr1e3gdI7Ib6XiCB8Ff5pbFEhvgrrU')
-
-print(bot.getMe())
-updates = bot.getUpdates()
-print([u.message.text for u in updates])
+print(user_bot.getMe())
+print(admin_bot.getMe())
+bot_updates = user_bot.getUpdates()
+admin_updates = admin_bot.getUpdates();
+print([u.message.text for u in bot_updates])
 
 questions = Commands.Questions
 
 
 def echo(bot, update):
-    print("Echo")
-    questions.result(bot, update)
+    #User
+    if bot.id == 210767489:
+        questions.result(bot, update)
+    #Admin
+    if bot.id == 224044671:
+        admin.result(bot, update)
+
 
 # from Commands import Basic
 
